3b62e7686a16221043124ece89f6b27147bcd8d9
xlr <iebulatov.ilya@gmail.com>
2026-05-09 12:29:34 +0500

    fix redis

--- redis/connection.py
+++ redis/connection.py
@@ -1,4 +1,10 @@
+import os
+
 import redis.asyncio
 
 # decode_responses=True makes Redis return strings instead of bytes
-r = redis.asyncio.Redis(host="localhost", port=6379, decode_responses=True)
+r = redis.asyncio.Redis(
+    host=os.getenv("REDIS_HOST", "localhost"),
+    port=int(os.getenv("REDIS_PORT", 6379)),
+    decode_responses=True,
+)
